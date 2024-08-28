799e99812ab8c55eb85abbf17213b89f507a447e
polanap <polanap@yandex.ru>
2024-08-28 14:34:42 +0300

    fixed styles

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
 
 </head>
 <body>
-        <table>
+        <table class="lay1">
                 <tr>
                         <th>
                                 <div class="container">
-                                        <h1>Labwork#1<sub>1234</sub></h1>
+                                        <h1>Labwork#1<sub>12345</sub></h1>
                                         <h2>Выполнила студент группы Р3109<br>Напольская Полина Александровна</br>
                                 </div>
                         </th>                        
@@ -34,61 +34,63 @@
                                 </div>
                         </td>
                         <td>
-                                <table>
-                                        <tr>
-                                                <div class="container">
-                                                        <form method="get" id="form">
-
-                                                                <label for="x">x:</label>
-                                                                <select type="button" id="x" name="x" required>
-                                                                                <option>-3</option>
-                                                                                <option>-2</option>
-                                                                                <option>-1</option>
-                                                                                <option>0</option>
-                                                                                <option>1</option>
-                                                                                <option>2</option>
-                                                                                <option>3</option>
-                                                                                <option>4</option>
-                                                                                <option>5</option>
-                                                                </select>
-
-                                                                <label for="y">y:</label>
-                                                                <input type="text" id="y" name="y" required>
-
-                                                                <label for="R">R:</label>
-                                                                <select type="radio" id="R" name="R" required>
-                                                                                <option>1</option>
-                                                                                <option>2</option>
-                                                                                <option>3</option>
-                                                                                <option>4</option>
-                                                                                <option>5</option>
-                                                                </select>
-
-                                                                <input type="submit" id="submitForm" value="Отправить">
-                                                
-                                                        </form>
-                                                </div>
-                                        </tr>
-                                        <tr>
-                                                <div class="container">
-                                                        <div class="values">
-                                                                <h3> -3 &le; x &le; 5</h3>
-                                                                <h3> -5 < y < 3</h3>
-                                                                <h3> 1 &le; R &le; 5</h3>
-                                                        </div>
-                                                </div>
-
-                                        </tr>
-                                </table>
-
-                        </td>
-                        <td>
                                 <div class="container">
-                                        <img src="resources\areas.png" alt="Area">
+                                        <table class="lay2">
+                                                <tr>
+                                                        <td>
+                                                        
+                                                                <form method="get" id="form">
+
+                                                                        <label for="x">x:</label>
+                                                                        <select type="button" id="x" name="x" required>
+                                                                                        <option>-3</option>
+                                                                                        <option>-2</option>
+                                                                                        <option>-1</option>
+                                                                                        <option>0</option>
+                                                                                        <option>1</option>
+                                                                                        <option>2</option>
+                                                                                        <option>3</option>
+                                                                                        <option>4</option>
+                                                                                        <option>5</option>
+                                                                        </select>
+
+                                                                        <label for="y">y:</label>
+                                                                        <input type="text" id="y" name="y" required>
+
+                                                                        <label for="R">R:</label>
+                                                                        <select type="radio" id="R" name="R" required>
+                                                                                        <option>1</option>
+                                                                                        <option>2</option>
+                                                                                        <option>3</option>
+                                                                                        <option>4</option>
+                                                                                        <option>5</option>
+                                                                        </select>
+
+                                                                        <input type="submit" id="submitForm" value="Отправить">
+                                                        
+                                                                </form>
+                                                        </td>
+                                                </tr>
+                                                <tr>
+                                                        <td>
+                                                        
+                                                                <div class="values">
+                                                                        <h3> -3 &le; x &le; 5; -5 < y < 3; 1 &le; R &le; 5</h3>
+                                                                </div>
+                                                        
+                                                        </td>
+                                                </tr>
+                                                <tr>
+                                                        <td>
+                                                                <img src="resources\areas.png" alt="Area">
+                                                        </td>
+                                                </tr>
+                                        </table>
                                 </div>
+
                         </td>
                 </tr>
         </table>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-                        <script type="text/javascript" src="validation.js"></script>
+        <script type="text/javascript" src="validation.js"></script>
 </body>
